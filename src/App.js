94bed657ea36424ae67746323a0e74bbc37e99c6
Chris Steffes <c.steffes@live.ca>
2021-06-13 15:07:28 -0500

    add state + gh button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Container, TextField, Button } from "@material-ui/core"
+import { Container, TextField, Button, Typography } from "@material-ui/core"
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import GitHubIcon from "@material-ui/icons/GitHub"
 import logo from "./mhlogo.svg"
 
 import React from "react"
 import "./App.css"
+import { useState } from "react"
 
 const theme = createMuiTheme({
   palette: {
@@ -16,19 +18,38 @@ const theme = createMuiTheme({
 })
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("0x0000000000000000000000000000000000000000")
+  const [walletBalance, setWalletBalance] = useState("0.00")
+
+  const onChangeWalletAddress = event => {
+    setWalletAddress(event.target.value)
+  }
+
+  const handleCheckBalance = () => {
+    console.log("Check Balance pushed")
+  }
+
+  const handleDonate = () => {
+    console.log("Donate pushed")
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
           <Container maxWidth="sm">
+            <Typography color="secondary" variant="h2" component="h2" gutterBottom>
+              {walletBalance} ETH
+            </Typography>
             <TextField
+              onChange={onChangeWalletAddress}
               autoFocus="true"
               id="outlined-full-width"
               label="Ethereum Wallet Address"
               style={{
                 margin: 8
               }}
-              placeholder="0x0000000000000000000000000000000000000000"
+              placeholder={walletAddress}
               fullWidth
               margin="normal"
               InputLabelProps={{
@@ -53,6 +74,7 @@ function App() {
             />
 
             <Button
+              onClick={handleCheckBalance(walletAddress)}
               style={{
                 margin: 8
               }}
@@ -63,16 +85,32 @@ function App() {
               Check Balance
             </Button>
             <Button
+              onClick={handleDonate}
               style={{
                 margin: 8
               }}
               size="medium"
               variant="outlined"
-              color="primary"
+              color="secondary"
             >
               Donate
             </Button>
           </Container>
+          <Button
+            startIcon={<GitHubIcon color="primary" />}
+            onClick={e => {
+              e.preventDefault()
+              window.location.href = "http://google.com"
+            }}
+            style={{
+              margin: 8
+            }}
+            size="small"
+            variant="outlined"
+            color="primary"
+          >
+            Github
+          </Button>
         </header>
       </div>
     </ThemeProvider>
