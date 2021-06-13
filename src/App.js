f221892786317d968d923897b344e21f1a10e5d3
Chris Steffes <c.steffes@live.ca>
2021-06-13 14:22:22 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, TextField, Button } from "@material-ui/core"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import logo from "./mhlogo.svg"
+
+import React from "react"
+import "./App.css"
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  },
+  typography: {
+    fontFamily: "Rajdhani",
+    fontSize: 19
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Container maxWidth="sm">
+            <TextField
+              autoFocus="true"
+              id="outlined-full-width"
+              label="Ethereum Wallet Address"
+              style={{
+                margin: 8
+              }}
+              placeholder="0x0000000000000000000000000000000000000000"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+                style: {
+                  fontWeight: 900
+                },
+                color: "secondary"
+              }}
+              InputProps={{
+                style: {
+                  color: "white"
+                }
+              }}
+              inputProps={{
+                style: {
+                  color: "white"
+                }
+              }}
+              variant="outlined"
+              color="secondary"
+            />
+
+            <Button
+              style={{
+                margin: 8
+              }}
+              size="medium"
+              variant="outlined"
+              color="secondary"
+            >
+              Check Balance
+            </Button>
+            <Button
+              style={{
+                margin: 8
+              }}
+              size="medium"
+              variant="outlined"
+              color="primary"
+            >
+              Donate
+            </Button>
+          </Container>
+        </header>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
