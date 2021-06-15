a9dbee00edaca41e0e97ee032608bda127c6512b
Chris Steffes <c.steffes@live.ca>
2021-06-14 20:14:11 -0500

    begining modal

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import { Container, TextField, Button, Typography } from "@material-ui/core"
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import GitHubIcon from "@material-ui/icons/GitHub"
-import logo from "./mhlogo.svg"
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Modal,
+} from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import logo from "./mhlogo.svg";
+import ModalComp from "./components/ModalComp";
 
-import React from "react"
-import "./App.css"
-import { useState } from "react"
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 // Material UI Tweaks
 const theme = createMuiTheme({
   palette: {
-    type: "dark"
+    type: "dark",
   },
   typography: {
     fontFamily: "Rajdhani",
-    fontSize: 20
-  }
-})
+    fontSize: 20,
+  },
+});
 
 // Web3 Setup
-require("dotenv").config()
-var Web3 = require("web3")
-const url = process.env["REACT_APP_URL"]
-console.log(url)
-var web3 = new Web3(url)
+require("dotenv").config();
+var Web3 = require("web3");
+const url = process.env["REACT_APP_URL"];
+console.log(url);
+var web3 = new Web3(url);
 
 function App() {
   // Declarign state variables for keeping track of wallet address and it's associated balance
-  const [walletAddress, setWalletAddress] = useState("0x0000000000000000000000000000000000000000")
-  const [walletBalance, setWalletBalance] = useState("0.00")
+  const [walletAddress, setWalletAddress] = useState(
+    "0x0000000000000000000000000000000000000000"
+  );
+  const [walletBalance, setWalletBalance] = useState("0.00");
 
   // Update the wallet address state anytime it changes
   function onChangeWalletAddress(event) {
-    setWalletAddress(event.target.value)
+    setWalletAddress(event.target.value);
   }
 
   // When button Check Balance Clicked
@@ -41,25 +50,25 @@ function App() {
       // Use web3 to get the balance from wallaetAddress (*state var)
       web3.eth.getBalance(walletAddress, (err, bal) => {
         // Use web3 conversion utility to get value in ETH
-        let balance = web3.utils.fromWei(bal)
-        setWalletBalance(balance)
+        let balance = web3.utils.fromWei(bal);
+        setWalletBalance(balance);
         // Show the error in the console
-        if (err) return console.log(err)
+        if (err) return console.log(err);
 
         // If there's no error, return no error and the balance
-        return null, bal
-      })
+        return null, bal;
+      });
     } else {
       // Some "error handling"
-      console.log("Inavlid Wallet Address")
+      console.log("Inavlid Wallet Address");
     }
-    console.log(`Wallet: ${walletAddress} has Balance: ${walletBalance}`)
+    console.log(`Wallet: ${walletAddress} has Balance: ${walletBalance}`);
   }
 
   // Handle the Donate button being clicked
   const handleDonate = () => {
-    console.log("Donate pushed")
-  }
+    alert("0x0A48F377D3004DD3687Fa0c93d61596F20476996");
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -67,7 +76,12 @@ function App() {
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="midheavy-logo" /> */}
           <Container maxWidth="sm">
-            <Typography color="secondary" variant="h3" component="h3" gutterBottom>
+            <Typography
+              color="secondary"
+              variant="h3"
+              component="h3"
+              gutterBottom
+            >
               {walletBalance} ETH
             </Typography>
             <TextField
@@ -76,7 +90,7 @@ function App() {
               id="outlined-full-width"
               label="Ethereum Wallet Address"
               style={{
-                margin: 8
+                margin: 8,
               }}
               placeholder={walletAddress}
               fullWidth
@@ -84,19 +98,19 @@ function App() {
               InputLabelProps={{
                 shrink: true,
                 style: {
-                  fontWeight: 900
+                  fontWeight: 900,
                 },
-                color: "secondary"
+                color: "secondary",
               }}
               InputProps={{
                 style: {
-                  color: "white"
-                }
+                  color: "white",
+                },
               }}
               inputProps={{
                 style: {
-                  color: "white"
-                }
+                  color: "white",
+                },
               }}
               variant="outlined"
               color="secondary"
@@ -105,7 +119,7 @@ function App() {
             <Button
               onClick={handleCheckBalance}
               style={{
-                margin: 8
+                margin: 8,
               }}
               size="medium"
               variant="outlined"
@@ -114,10 +128,9 @@ function App() {
               Check Balance
             </Button>
             <Button
-              disabled
               onClick={handleDonate}
               style={{
-                margin: 8
+                margin: 8,
               }}
               size="medium"
               variant="outlined"
@@ -125,15 +138,17 @@ function App() {
             >
               Donate
             </Button>
+            {/* <ModalComp /> */}
           </Container>
           <Button
             startIcon={<GitHubIcon color="primary" />}
             onClick={e => {
-              e.preventDefault()
-              window.location.href = "https://github.com/Pandaphobic/react-material-web3"
+              e.preventDefault();
+              window.location.href =
+                "https://github.com/Pandaphobic/react-material-web3";
             }}
             style={{
-              margin: 8
+              margin: 8,
             }}
             size="small"
             variant="outlined"
@@ -149,7 +164,7 @@ function App() {
               position: "absolute",
               fontSize: ".5em",
               fontWeight: "400",
-              bottom: 0
+              bottom: 0,
             }}
           >
             Chris Steffes - chrissteffes.crypto
@@ -157,7 +172,7 @@ function App() {
         </header>
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
