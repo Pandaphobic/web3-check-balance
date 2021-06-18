80e55a97d9c8fd248d3b6c40fe6abb2ec6863d38
Chris Steffes <c.steffes@live.ca>
2021-06-18 15:34:35 -0500

    moved everythig to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Container, TextField, Button, Typography } from "@material-ui/core"
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import GitHubIcon from "@material-ui/icons/GitHub"
-import { OnboardingButton } from "./components/OnboardingButton"
-
 import React from "react"
 import "./App.css"
 import { useState } from "react"
+
+// EXPERIMENTAL
+// import { Button } from "@material-ui/core"
+import MetaMaskOnboarding from "@metamask/onboarding"
+// import React from "react"
+const ONBOARD_TEXT = "Click here to install MetaMask!"
+const CONNECT_TEXT = "Connect"
+const CONNECTED_TEXT = "Connected"
 
 // Material UI Tweaks
 const theme = createMuiTheme({
@@ -18,12 +24,73 @@ const theme = createMuiTheme({
   }
 })
 
-// // Web3 Setup
-// require("dotenv").config()
-// var Web3 = require("web3")
-// const url = process.env["REACT_APP_URL"]
+// Web3 Setup
+require("dotenv").config()
+var Web3 = require("web3")
+const url = process.env["REACT_APP_URL"]
 // console.log(url)
-// var web3 = new Web3(url)
+var web3 = new Web3(url)
+
+function OnboardingButton() {
+  const [buttonText, setButtonText] = React.useState(ONBOARD_TEXT)
+  const [isDisabled, setDisabled] = React.useState(false)
+  const [accounts, setAccounts] = React.useState([])
+  const onboarding = React.useRef()
+
+  React.useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding()
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(CONNECTED_TEXT)
+        setDisabled(true)
+        onboarding.current.stopOnboarding()
+      } else {
+        setButtonText(CONNECT_TEXT)
+        setDisabled(false)
+      }
+    }
+  }, [accounts])
+
+  React.useEffect(() => {
+    function handleNewAccounts(newAccounts) {
+      setAccounts(newAccounts)
+    }
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum.request({ method: "eth_requestAccounts" }).then(handleNewAccounts)
+      window.ethereum.on("accountsChanged", handleNewAccounts)
+      return () => {
+        window.ethereum.off("accountsChanged", handleNewAccounts)
+      }
+    }
+  }, [])
+
+  const onClick = () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum.request({ method: "eth_requestAccounts" }).then(newAccounts => setAccounts(newAccounts))
+    } else {
+      onboarding.current.startOnboarding()
+    }
+  }
+  return (
+    <Button
+      style={{
+        margin: 8
+      }}
+      size="medium"
+      variant="outlined"
+      color="secondary"
+      disabled={isDisabled}
+      onClick={onClick}
+    >
+      {buttonText}
+    </Button>
+  )
+}
 
 function App() {
   // Declarign state variables for keeping track of wallet address and it's associated balance
@@ -37,27 +104,22 @@ function App() {
 
   // When button Check Balance Clicked
   function handleCheckBalance() {
-    // if (web3.utils.isAddress(walletAddress)) {
-    //   // Use web3 to get the balance from wallaetAddress (*state var)
-    //   web3.eth.getBalance(walletAddress, (err, bal) => {
-    //     // Use web3 conversion utility to get value in ETH
-    //     let balance = web3.utils.fromWei(bal)
-    //     setWalletBalance(balance)
-    //     // Show the error in the console
-    //     if (err) return console.log(err)
-    //     // If there's no error, return no error and the balance
-    //     return null, bal
-    //   })
-    // } else {
-    //   // Some "error handling"
-    //   console.log("Inavlid Wallet Address")
-    // }
-    // console.log(`Wallet: ${walletAddress} has Balance: ${walletBalance}`)
-  }
-
-  // Handle the Donate button being clicked
-  const handleDonate = () => {
-    // alert("0x0A48F377D3004DD3687Fa0c93d61596F20476996");
+    if (web3.utils.isAddress(walletAddress)) {
+      // Use web3 to get the balance from wallaetAddress (*state var)
+      web3.eth.getBalance(walletAddress, (err, bal) => {
+        // Use web3 conversion utility to get value in ETH
+        let balance = web3.utils.fromWei(bal)
+        setWalletBalance(balance)
+        // Show the error in the console
+        if (err) return console.log(err)
+        // If there's no error, return no error and the balance
+        return bal
+      })
+      console.log(`Wallet: ${walletAddress} has Balance: ${walletBalance}`)
+    } else {
+      // Some "error handling"
+      console.log("Inavlid Wallet Address")
+    }
   }
 
   return (
@@ -139,7 +201,6 @@ function App() {
               fontWeight: "900",
               position: "absolute",
               fontSize: ".5em",
-              fontWeight: "400",
               bottom: 0
             }}
           >
